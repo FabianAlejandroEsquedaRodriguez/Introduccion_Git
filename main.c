fbf88791ca07115ac80cfb9bc486c69d1c641b10
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2020-09-15 13:45:57 -0500

    Leimos el nombre completo del usuario e imprimimos el nombre del saludo

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
+    char nombre[100];
     printf("Hola mundo!");
+
+    printf("\n\nNombre: ");
+    fgets(nombre, sizeof(nombre), stdin);//Donde se va a guardar eso que vamos a leer
+                                        //Hasta cuanto vamos a leer
+                                        //Desde la entrada de la terminal
+
+    printf("Hola %s", nombre);
 
     return 0;
 }
